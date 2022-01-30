ccbf7f18ba11e6d33903d3f1d8abd6d7e9d0b4a2
Алексей <defers86@gmail.com>
2022-01-30 23:27:05 +0300

    first commit

---
+++ src/main/java/com/defers/taskmanager/entity/Task.java
@@ -0,0 +1,29 @@
+package com.defers.taskmanager.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "creation_date")
+    private Date date;
+
+}
---
+++ src/test/java/com/defers/taskmanager/entity/TaskTest.java
@@ -0,0 +1,30 @@
+package com.defers.taskmanager.entity;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+
+
+@SpringBootTest
+class TaskTest {
+
+    @Test
+    void shouldCreateClassWithAllArguments() {
+
+        Date date = new GregorianCalendar(2022, 01, 15).getTime();
+        Task task = new Task(1L, "some info", date);
+
+        assertThat(task).isNotNull();
+        assertThat(task.getId()).isEqualTo(1L);
+        assertThat(task.getDescription()).isEqualTo("some info");
+        assertThat(task.getDate()).isEqualTo(date);
+
+    }
+
+
+}
